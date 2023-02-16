b164cc2d875b8d375496707247fa5ceedc8a57eb
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2023-02-17 00:36:16 +0530

    GFG Important

---
+++ Rod Cutting/Rod Cutting.java
@@ -0,0 +1,11 @@
+class Solution{
+    public int cutRod(int price[], int n) {
+     int []dp = new int[n+1] ;
+     for(int i = 1 ;i<=n;i++){
+         for(int j = 0 ;j<i;j++){
+             dp[i]= Math.max(dp[i],price[j]+dp[i-j-1]) ;
+         }
+     }
+     return dp[n] ;
+    }
+}
