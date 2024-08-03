8c23ffcb0a95042dda7d1106ebef9e942fd5cd66
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2024-08-03 17:25:20 +0530

    GFG Important

---
+++ Mirror Tree/Mirror Tree.java
@@ -0,0 +1,24 @@
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
+
+class Solution {
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        if(node ==null)return ;
+        
+        mirror(node.left);
+        mirror(node.right);
+        
+        Node temp = node.left ;
+        node.left =node.right ;
+        node.right = temp ;
+    }
+}
