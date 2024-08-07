8c5bcb451b74f696ce3451ba0fde9904c2c6800a
Kishan Kumar Pandey <117659255+Kishanpandey2@users.noreply.github.com>
2024-08-07 23:29:12 +0530

    GFG Important

---
+++ Identical Trees/Identical Trees.java
@@ -0,0 +1,16 @@
+
+class Solution
+{
+    
+	boolean isIdentical(Node root1, Node root2)
+	{
+	    if(root1 == null && root2 == null)return true ;
+	 
+	  if(root1!=null && root2!= null)
+	    return root1.data == root2.data && isIdentical(root1.left,root2.left) && isIdentical(root1.right,root2.right) ;
+	  
+	    return false;
+	    
+	}
+	
+}
